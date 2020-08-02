1cff997c3287b3026e3270c68d7457d7c3e0c011
Mo Yuli <moyuli@sensetime.com>
2020-08-02 10:12:13 +0800

    feat: add row definition

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,37 @@ use std::fs;
 use std::io::{BufRead, BufReader};
 use std::path::Path;
 
+/* Individual color handles. */
+static COLOR_BACKGROUND: i16 = 16;
+static COLOR_FOREGROUND: i16 = 17;
+static COLOR_KEYWORD: i16 = 18;
+static COLOR_TYPE: i16 = 19;
+static COLOR_STORAGE: i16 = 20;
+static COLOR_COMMENT: i16 = 21;
+static COLOR_STRING: i16 = 22;
+static COLOR_CHAR: i16 = 23;
+static COLOR_NUMBER: i16 = 24;
+
+/* Color pairs; foreground && background. */
+static COLOR_PAIR_DEFAULT: i16 = 1;
+static COLOR_PAIR_KEYWORD: i16 = 2;
+static COLOR_PAIR_TYPE: i16 = 3;
+static COLOR_PAIR_STORAGE: i16 = 4;
+static COLOR_PAIR_COMMENT: i16 = 5;
+static COLOR_PAIR_STRING: i16 = 6;
+static COLOR_PAIR_CHAR: i16 = 7;
+static COLOR_PAIR_NUMBER: i16 = 8;
+
+/* Word delimiters. */
+static WORD_LIMITS: &'static [u8] = &[
+    ' ' as u8, '(' as u8, ')' as u8, ':' as u8, ';' as u8, '&' as u8, '+' as u8, '-' as u8,
+    ',' as u8, '.' as u8, '@' as u8, '~' as u8, '\\' as u8, '\n' as u8, '\r' as u8, '\0' as u8,
+    !0 as u8,
+];
+
 #[derive(Debug, Default)]
 struct Context {
-    pub lines: Vec<String>,
+    pub rows: Vec<Row>,
     pub scr_width: i32,
     pub scr_height: i32,
     pub buf_length: i32,
@@ -16,30 +44,39 @@ struct Context {
     pub y_offset: i32,
 }
 
-fn read_lines() -> Vec<String> {
+#[derive(Debug, Default)]
+struct Row {
+    pub raw: String,
+    pub tokens: Vec<(String, nc::attr_t)>,
+}
+
+fn read_rows() -> Vec<Row> {
     let args: Vec<_> = env::args().collect();
     if args.len() != 2 {
         println!("Usage:\n\t{} <file>", args[0]);
         std::process::exit(1)
     }
 
-    let mut lines: Vec<String> = vec![];
+    let mut rows: Vec<Row> = vec![];
     let file = fs::File::open(Path::new(&args[1])).expect("unable to open file");
     let buffer = BufReader::new(file).lines();
     for line in buffer {
         if let Ok(l) = line {
-            lines.push(l);
+            rows.push(Row {
+                raw: l,
+                tokens: vec![],
+            });
         }
     }
 
-    lines
+    rows
 }
 
 fn add_line(ctx: &mut Context, i: i32) -> bool {
-    if ctx.y_offset + i >= ctx.lines.len() as i32 {
+    if ctx.y_offset + i >= ctx.rows.len() as i32 {
         return false;
     }
-    let s = &ctx.lines[(ctx.y_offset + i) as usize];
+    let s = &ctx.rows[(ctx.y_offset + i) as usize].raw;
     for c in s.chars() {
         let mut cur_x = 0;
         let mut cur_y = 0;
@@ -78,7 +115,7 @@ fn prompt(ctx: &Context) {
 }
 
 fn main() {
-    let lines = read_lines();
+    let lines = read_rows();
 
     nc::initscr();
     nc::keypad(nc::stdscr(), true);
@@ -87,7 +124,7 @@ fn main() {
 
     let mut ctx = Context::default();
     ctx.buf_length = lines.len() as i32;
-    ctx.lines = lines;
+    ctx.rows = lines;
     nc::getmaxyx(nc::stdscr(), &mut ctx.scr_height, &mut ctx.scr_width);
 
     loop {
